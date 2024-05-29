5bfb782aff289ed8a3cbab6dd24f6a76eb56e2f2
Daniel B <128756372+Daniel-B-Oliveira@users.noreply.github.com>
2024-05-28 22:21:40 -0300

    Primeira aula

---
+++ aula_1.R
@@ -0,0 +1,29 @@
+1+1 #soma
+1-10 #subtracao
+5*76 #multiplicacao
+12/5 #divisao
+3**4 #potencia
+3^4 #potencia
+
+a <- 12/5
+b <- a*5
+
+32+345 -> numero1
+class(numero1)
+
+numero2 = 32+345 #não usar de preferencia
+
+w <- "ciencia"
+class(w)
+
+x <- "3"
+class(x)
+
+y <- TRUE
+class(y)
+y + 10
+
+z <- FALSE
+class(z)
+z+10
+
