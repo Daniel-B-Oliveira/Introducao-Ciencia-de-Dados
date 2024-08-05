bbb4b9a57187dacdd476f553acf65e7dafa42758
Daniel B. <128756372+Daniel-B-Oliveira@users.noreply.github.com>
2024-08-05 19:38:36 -0300

    Aula 5

---
+++ aula_5.R
@@ -0,0 +1,13 @@
+#Revisão
+  #Operações básicas
+  #Objetos
+  #Funções: print, class, seq, sample
+  #Vetores: c()
+
+seq(from = 4, to = 77)
+
+sorteio <- sample(x = 1:60, size = 6, replace = FALSE)
+bilhete <- c(10,20,30,40,50,60)
+
+bilhete %in% sorteio
+sum(bilhete %in% sorteio)
